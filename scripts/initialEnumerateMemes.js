de63dfac8dff2f6775a9777ac88ff6e3c91ca03c
Jaspreet Singh <jaspreet@joinhoney.com>
2017-11-20 18:03:54 -0800

    feat(memes): Add memes and generation script

---
+++ scripts/initialEnumerateMemes.js
@@ -0,0 +1,17 @@
+const { readdirSync, copyFileSync } = require('fs')
+const { extname } = require('path')
+const pad = require('pad')
+
+const SRC = './dank-memes'
+const DIST = './dank-memes-enumerated'
+
+const srcMemes = readdirSync(SRC)
+
+let idx = 0;
+for (const srcMeme of srcMemes) {
+  const ext = extname(srcMeme)
+  const paddedId = pad(5, `${idx++}`, '0')
+  const newFileName = `DM-${paddedId}${ext}`
+  console.log(`Generating ${DIST}/${newFileName}`)
+  copyFileSync(`${SRC}/${srcMeme}`, `${DIST}/${newFileName}`)
+}
