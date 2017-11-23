9044275864303ad8744b4fe738c8e20207f9b270
Jaspreet Singh <jaspreet@joinhoney.com>
2017-11-22 16:30:55 -0800

    feat(longterm-caching): Don't update the vendor bundle so long term caching actually works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,9 @@
 
 npm run build:client:prod
 node scripts/dropGzExt.js
-aws s3 rm --recursive s3://topkekkle.com
+aws s3 rm s3://topkekkle.com/index*
+aws s3 rm s3://topkekkle.com/manifest*
+aws s3 rm s3://topkekkle.com/bundle*
+rm dist/vendor*
 aws s3 cp --recursive --content-encoding gzip dist s3://topkekkle.com
 
