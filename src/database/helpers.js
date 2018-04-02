72ddbede77fcac60ef808bd321c37dfdb1d04486
Jaspreet Singh <jaspreet@joinhoney.com>
2018-04-02 13:30:29 -0700

    feat(s3): Move off of imgix

--- src/database/helpers.js
+++ src/database/helpers.js
@@ -1,17 +1,19 @@
-const generateFullURL = (imageId) => `https://topkekkle.imgix.net/enumerated/${imageId}`
+const base = 'https://s3-us-west-1.amazonaws.com/top-kekkle';
+
+const generateFullURL = (imageId) => `${base}/enumerated/${imageId}`;
 
 const generateThumbnailURL = (imageId) => {
-  const [name, ext] = imageId.split('.')
-  const thumbnailFile = `${name}-T.${ext}`
-  return `https://topkekkle.imgix.net/enumerated-thumbnails/${thumbnailFile}`
-}
+  const [name, ext] = imageId.split('.');
+  const thumbnailFile = `${name}-T.${ext}`;
+  return `${base}/enumerated-thumbnails/${thumbnailFile}`;
+};
 
 export const generateAdditionalFields = (imageId) => {
-  const thumbnailURL = generateThumbnailURL(imageId)
-  const fullURL = generateFullURL(imageId)
+  const thumbnailURL = generateThumbnailURL(imageId);
+  const fullURL = generateFullURL(imageId);
   return {
     thumbnailURL,
     fullURL
-  }
-}
+  };
+};
 
